5738c9e509df203f78d95bc8007f42aebb0da2e1
azurecurve <github@azurecurve.co.uk>
2024-02-03 15:07:19 +0000

    Release v1.0.12

--- azrcrv-redirect.php
+++ azrcrv-redirect.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * ------------------------------------------------------------------------------
- * Plugin Name: Redirect
- * Description: Redirect URIs with a 301 (permanent) or 302 (temporary) redirect.
- * Version: 1.0.11
- * Author: azurecurve
- * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
- * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/redirect/
- * Text Domain: azrcrv-r
- * Domain Path: /languages
+ * Plugin Name:		Redirect
+ * Description:		Redirect URIs with a 301 (permanent) or 302 (temporary) redirect.
+ * Version:			1.0.12
+ * Requires CP:		1.0
+ * Author:			azurecurve
+ * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/redirect/
+ * Donate link:		https://development.azurecurve.co.uk/support-development/
+ * Text Domain:		azrcrv-r
+ * Domain Path:		/languages
+ * License:			GPLv2 or later
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.html
  * ------------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
